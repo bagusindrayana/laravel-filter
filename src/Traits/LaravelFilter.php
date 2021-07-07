d84b25702d3242768a128f82e9f5bb22ce8b123e
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2021-07-07 15:35:33 +0800

    add or scope and base loop scope

--- src/Traits/LaravelFilter.php
+++ src/Traits/LaravelFilter.php
@@ -6,18 +6,9 @@ trait LaravelFilter
 {   
     // public $filterFields = [];
 
-    protected function scopeFiltersInput($query,$fields = null,$input = 'cari')
-    {   
-        $fields = $fields ?? $this->filterFields;
-        $query = $query->filters(request()->$input,$fields);
-        
-        return $query;
-    }
-
-    protected function scopeFilters($query,$input,$fields = null)
-    {   
-        
-        $fields = $fields ?? $this->filterFields;
+    //base loop where
+    protected function scopeFilter($query,$input,$fields)
+    {
         $query = $query->where(function($model)use($fields,$input){
             foreach($fields as $i => $field){
                 if(is_array($field)){
@@ -35,6 +26,76 @@ trait LaravelFilter
                         $model = $model->where($field,'LIKE','%'.$input.'%');
                     }
                 }
+            }
+        });
+    }
+
+    //filter with input
+    protected function scopeFiltersInput($query,$fields = null,$input = 'cari')
+    {   
+        $fields = $fields ?? $this->filterFields;
+        
+        $query = $query->where(function($q)use($fields,$input){
+            if(is_array($input)){
+                foreach ($input as $key => $value) {
+                    $q = $q->orFilters(request()->$value,$fields);
+                }
+            } else {
+                $q = $q->filters(request()->$input,$fields);
+            }
+        });
+        
+        return $query;
+    }
+
+    //filter with value
+    protected function scopeFilters($query,$input,$fields = null)
+    {   
+        
+        $fields = $fields ?? $this->filterFields;
+        $query = $query->where(function($q)use($fields,$input){
+            if(is_array($input)){
+                foreach ($input as $key => $value) {
+                    $q = $q->orFilters($value,$fields);
+                }
+            } else {
+                $q = $q->filter($input,$fields);
+            }
+        });
+        return $query;
+    }
+
+
+    //or filter with input
+    protected function scopeOrFiltersInput($query,$fields = null,$input = 'cari')
+    {   
+        $fields = $fields ?? $this->filterFields;
+        
+        $query = $query->orWhere(function($q)use($fields,$input){
+            if(is_array($input)){
+                foreach ($input as $key => $value) {
+                    $q = $q->orFilters(request()->$value,$fields);
+                }
+            } else {
+                $q = $q->orFilters(request()->$input,$fields);
+            }
+        });
+        
+        return $query;
+    }
+
+    //or filter with value
+    protected function scopeOrFilters($query,$input,$fields = null)
+    {   
+        
+        $fields = $fields ?? $this->filterFields;
+        $query = $query->orWhere(function($q)use($fields,$input){
+            if(is_array($input)){
+                foreach ($input as $key => $value) {
+                    $q = $q->orFilters($value,$fields);
+                }
+            } else {
+                $q = $q->filter($input,$fields);
             }
         });
         return $query;
